4b159f7df75f401cefcdd5824ffd071d299e4f18
bharat <bharat2608patil@gmail.com>
2026-09-15 11:37:40 +0530

    first commit

---
+++ src/main/java/com/wsr_management_service/WsrManagementServiceApplication.java
@@ -0,0 +1,14 @@
+package com.wsr_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class WsrManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsrManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wsr_management_service/WsrManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsr_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsrManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
